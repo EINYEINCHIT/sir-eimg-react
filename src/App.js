6d6d9474b8ae750adffca602a92d47a1507a0fff
Ei Ei <ei.nyein.chit@chanjao.com>
2025-11-15 04:28:14 +0630

    react data flow

--- src/App.js
+++ src/App.js
@@ -1,65 +1,29 @@
 import React from "react";
 
-class Item extends React.Component {
+class Title extends React.Component {
   render() {
-    return (
-      <li>
-        {this.props.name},&nbsp;{this.props.price}
-      </li>
-    );
+    return <h1>{this.props.name}</h1>;
   }
 }
-
-class App extends React.Component {
-  state = {
-    items: [
-      { id: 1, name: "Apple", price: 0.99 },
-      { id: 2, name: "Orange", price: 0.89 },
-    ],
-  };
-
-  nameRef = React.createRef();
-  priceRef = React.createRef();
-
-  add = () => {
-    let id = this.state.items.length + 1;
-    let name = this.nameRef.current.value;
-    let price = this.priceRef.current.value;
-
-    this.setState({
-      items: [
-        ...this.state.items,
-        {
-          id,
-          name,
-          price
-        },
-      ],
-    });
-  };
-
+class Header extends React.Component {
   render() {
     return (
       <div>
-       
-        <h1>Hello React</h1>
-        <ul>
-          {this.state.items.map((i) => {
-            return (
-              <Item
-                key={i.id}
-                name={i.name}
-                price={i.price}
-              />
-            )
-          })}
-        </ul>
-        Name: <input type="text" ref={this.nameRef} /><br />
-        Price: <input type="text" ref={this.priceRef} /><br />
-        <button onClick={this.add}>Add</button>
+        <Title name={this.props.name} />
+      </div>
+    );
+  }
+}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header name="App Title" />
+        ...
       </div>
     );
   }
 }
 
 export default App;
+
